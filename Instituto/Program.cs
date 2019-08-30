5cee0bbbc11c6d0cc31827aadc5bc3d56aa464eb
RicardoMareco <ricardomareco99@gmail.com>
2019-08-30 19:56:06 -0400

    Agregué un saludo

--- Instituto/Program.cs
+++ Instituto/Program.cs
@@ -10,6 +10,8 @@ namespace Instituto
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola soy Ricardo! uwu");
+            Console.ReadLine();
         }
     }
 }
